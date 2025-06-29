6ce81322c4d0b93aa1f703173967cb2cbb40f40c
Ifti-Alamin <iftialmin10@gmail.com>
2025-06-29 16:52:06 +0600

    card added

--- Building frontend/frontEnd/src/user/components/UserItem.tsx
+++ Building frontend/frontEnd/src/user/components/UserItem.tsx
@@ -1,20 +1,24 @@
+import { Link } from "react-router-dom";
+import type { UserItemProps } from "../../type";
+
 import Avatar from "../../shared/components/UIElements/Avatar";
 import "./UserItem.css";
-import type { UserItemProps } from "../../type";
 
 const UserItem = (props: UserItemProps) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}{" "}
-          </h3>
-        </div>
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}{" "}
+            </h3>
+          </div>
+        </Link>
       </div>
     </li>
   );
