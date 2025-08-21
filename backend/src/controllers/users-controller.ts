5a78aa0824ce66912776d67344891a3e2b74889a
Ifti-Alamin <iftialmin10@gmail.com>
2025-08-21 15:37:08 +0600

    successfully User login Implemented with DB

--- backend/src/controllers/users-controller.ts
+++ backend/src/controllers/users-controller.ts
@@ -78,16 +78,26 @@ const signup = async (req: Request, res: Response, next: NextFunction) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req: Request, res: Response, next: NextFunction) => {
+const login = async (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-
-  if (!identifiedUser || identifiedUser.password !== password) {
-    next(new HttpError("Could not match with credential.", 401));
-  } else {
-    res.json({ message: "Logged in!" });
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Logging in failed, Please try later", 500);
+    return next(error);
   }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
+    );
+    return next(error);
+  }
+
+  res.json({ message: "Logged in!" });
 };
 
 export { getUsers, signup, login };
